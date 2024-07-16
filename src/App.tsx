7d6d755375367b5c9a046cef461af505de3f2cfe
ryuunderground <jaeking00001@gmail.com>
2024-07-16 17:42:17 +0900

    Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
+import { useState } from "react";
 import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
+
+const Container = styled.div`
+  display: flex;
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const Title = styled.h1`
+  font-size: 12px;
+  color: ${(props) => props.theme.textColor};
+`;
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const naming = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setUsername(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello", username);
+  };
+
   return (
     <>
-      <Circle bgColor="teal" borderColor="red"></Circle>
-      <Circle bgColor="orange"></Circle>
+      <Container>
+        <Title>Hello</Title>
+      </Container>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          onChange={naming}
+          value={username}
+        />
+        <button>Log in</button>
+      </form>
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={darkTheme}>
+    <App />
+  </ThemeProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
