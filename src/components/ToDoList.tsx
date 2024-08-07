66287617a018debed4861837645d9764be2c30eb
ryuunderground <jaeking00001@gmail.com>
2024-08-07 11:58:27 +0900

    react-hook-form

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-const ToDoList = () => {
+/* const ToDoList = () => {
   const [toDo, setTodo] = useState("");
   const [toDoErr, setToDoErr] = useState("");
 
@@ -28,6 +29,81 @@ const ToDoList = () => {
         <button>Add</button>
       </form>
       {toDoErr !== "" ? toDoErr : null}
+    </div>
+  );
+}; */
+
+interface IForm {
+  Email: string;
+  firstPhone?: string;
+  secondPhone: number;
+  lastPhone: number;
+  password: string;
+}
+
+const ToDoList = () => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      Email: "@naver.com",
+      firstPhone: "010",
+    },
+  });
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(watch());
+  console.log(errors);
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register("Email", {
+            required: "Write Email",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Naver email Only",
+            },
+          })}
+          placeholder="Write Email"
+        />
+        <span>{errors?.Email?.message?.toString()}</span>
+        <input {...register("firstPhone")} placeholder="Write" />
+        <input
+          {...register("secondPhone", {
+            required: true,
+            minLength: {
+              value: 4,
+              message: "fullfill the requirement",
+            },
+            maxLength: {
+              value: 4,
+              message: "fullfill the requirement",
+            },
+          })}
+          placeholder="Phone"
+        />
+        <span>{errors?.secondPhone?.message?.toString()}</span>
+        <input
+          {...(register("lastPhone"),
+          { required: true, minLength: 4, maxLength: 4 })}
+          placeholder="Number"
+        />
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: 8,
+            maxLength: 16,
+          })}
+          placeholder="Write password"
+        />
+        <span>{errors?.lastPhone?.message?.toString()}</span>
+        <button>Add</button>
+      </form>
     </div>
   );
 };
