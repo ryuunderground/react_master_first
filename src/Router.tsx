dff738c037f16a4c2f1ebf9d21162d44ed5e8582
ryuunderground <jaeking00001@gmail.com>
2025-01-05 23:38:44 +0900

    route

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,7 +10,7 @@ import ReviewScreen from "./Screen/ReviewScreen";
 
 const router = createBrowserRouter([
   {
-    path: "/react_master_graduate/",
+    path: "/react_master",
     element: <App />,
     children: [
       {
